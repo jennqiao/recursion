e2b01eee106f0c8ee9276425f16c2d56cc36ddee
Jennifer Qiao <jennifercqiao@gmail.com>
2018-02-14 13:36:34 -0800

    Works for tests up to arrays for jsonstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,70 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //base case:
+  //recursion case: pop out
+  //check if number, boolean, null, array, or obj?
+
+  
+
+  if (typeof(obj)==="number" || typeof(obj)==="boolean" || typeof(obj)==="undefined"  || obj===null) {
+  	var stringVersion = String(obj);
+  	return stringVersion;
+  }
+
+ else if (typeof(obj)==="string") {
+ 	return '"'+obj+'"';
+ } 
+
+ 
+ else if (Array.isArray(obj)) {
+
+ 	if (obj.length > 0) {
+ 		/*
+ 		 return '['+_.reduce(obj, function(accumulator, item) {
+ 		 	if (obj.length >1) {
+ 		 		return accumulator+','+stringifyJSON(item);
+
+ 		 	}
+ 		 	else {
+ 		 		return stringifyJSON(accumulator);
+ 		 	}
+ 		 })+']';
+ 		 
+		*/
+ 		 
+
+ 		 var newString = '[';
+ 		 for (var i=0; i<obj.length; i++) {
+ 		 	newString+=stringifyJSON(obj[i]);
+ 		 	if (obj.length > 1 && i != obj.length-1) {
+
+
+ 		 		newString+=",";
+ 		 	}
+ 		 }
+ 		 newString += ']';
+ 		 return newString;
+
+ 		 
+
+ 	}
+ 	else {
+ 		return '[]';
+ 	}
+ }
+
+
+
+
+
+
+ 
+
+
 };
+
+
+function isObject(obj) {
+  return obj === Object(obj);
+}
